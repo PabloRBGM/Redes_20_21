38505ae4b3e118fe0421c8093ef87a102969b077
PabloRBGM <prodra02@ucm.es>
2021-05-07 12:17:23 +0200

    Inicio con código de clase

--- P1/ejercicio1.cc
+++ P1/ejercicio1.cc
@@ -1,1 +1,29 @@
 // Ejercicio 1
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <iostream>
+
+int main(int argc, char** argv){
+    addrinfo hints;
+    addrinfo *res;
+
+    memset((void*) &hints, 0, sizeof(addrinfo));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+
+    if(rc != 0){
+        std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << '\n';
+        return -1;
+    }
+
+    int sd = socket(res->ai_family, res->ai_socktype, 0);
+
+    freeaddrinfo(res);
+
+    return 0;
+}
